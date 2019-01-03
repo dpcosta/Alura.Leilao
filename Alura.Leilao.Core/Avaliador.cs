d46355cf7d4a1889cc493cdf5b8eaf318b901c8a
Daniel Portugal <dpcosta@gmail.com>
2019-01-03 16:42:12 -0200

    Criação da história de avaliação do leilão. Cenário de teste usando um programa console. Existem cenários que quebram o avaliador?

---
+++ Alura.Leilao.Core/Avaliador.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Alura.Leilao.Core
+{
+    public class Avaliador
+    {
+        public Leilao Leilao { get; }
+        public double MaiorLance { get; private set; } = double.MinValue;
+        public double MenorLance { get; private set; } = double.MaxValue;
+
+        public Avaliador(Leilao leilao)
+        {
+            Leilao = leilao;
+        }
+
+        public void Avalia()
+        {
+            foreach (var lance in Leilao.Lances)
+            {
+                if (lance.Valor > MaiorLance)
+                {
+                    MaiorLance = lance.Valor;
+                }
+                else if (lance.Valor < MenorLance)
+                {
+                    MenorLance = lance.Valor;
+                }
+            }
+        }
+
+
+    }
+}
