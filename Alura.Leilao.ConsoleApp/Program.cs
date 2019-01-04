1c2f5e03bf75b3fed154f77e5d5389dadbd4aecc
Daniel Portugal <dpcosta@gmail.com>
2019-01-04 16:58:55 -0200

    Registrando as motivações para passar de testes manuais para automatizados.

--- Alura.Leilao.ConsoleApp/Program.cs
+++ Alura.Leilao.ConsoleApp/Program.cs
@@ -10,28 +10,61 @@ namespace Alura.Leilao.ConsoleApp
 
         static void Verifica(double valorEsperado, double valorRetornado)
         {
+            var corAnterior = Console.ForegroundColor;
             if (Math.Abs(valorEsperado - valorRetornado) < 0.0001)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(MSG_OK, valorEsperado);
+            }
             else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(MSG_FALHA, valorEsperado, valorRetornado);
+            }
+            Console.ForegroundColor = corAnterior;
         }
 
-        static void Main()
+        static void AvaliaLeilaoComTresLances()
         {
+            Console.WriteLine(nameof(AvaliaLeilaoComTresLances));
             var joao = new Cliente("João de Miranda");
             var pedro = new Cliente("Pedro Silveira");
             var malu = new Cliente("Malu Pereira");
             var leilao = new Alura.Leilao.Core.Leilao("Obra de Rembrant");
-            leilao.Propoe(new Lance(joao, 1200));
             leilao.Propoe(new Lance(pedro, 1300));
             leilao.Propoe(new Lance(malu, 1350));
+            leilao.Propoe(new Lance(joao, 1200));
 
             var leiloeiro = new Avaliador(leilao);
             leiloeiro.Avalia();
 
             Verifica(1350, leiloeiro.MaiorLance);
-            Verifica(1200, leiloeiro.MenorLance);
+            Verifica(900, leiloeiro.MenorLance);
+            Console.WriteLine("");
+        }
 
+        static void AvaliaLeilaoComLancesDoMesmoCliente()
+        {
+            Console.WriteLine(nameof(AvaliaLeilaoComLancesDoMesmoCliente));
+            var joao = new Cliente("João de Miranda");
+            var leilao = new Alura.Leilao.Core.Leilao("Obra de Dalí");
+            leilao.Propoe(new Lance(joao, 900));
+            leilao.Propoe(new Lance(joao, 1200));
+            leilao.Propoe(new Lance(joao, 1300));
+            leilao.Propoe(new Lance(joao, 1350));
+
+            var leiloeiro = new Avaliador(leilao);
+            leiloeiro.Avalia();
+
+            Verifica(1350, leiloeiro.MaiorLance);
+            Verifica(900, leiloeiro.MenorLance);
+            Console.WriteLine("");
+        }
+
+        static void Main()
+        {
+            AvaliaLeilaoComTresLances();
+            AvaliaLeilaoComLancesDoMesmoCliente();
         }
     }
 }
---
+++ Alura.Leilao.ConsoleApp/TesteSemAssert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Alura.Leilao.Core;
+
+namespace Alura.Leilao.ConsoleApp
+{
+    public class TesteSemAssert
+    {
+        static void Main()
+        {
+            var joao = new Cliente("João de Miranda");
+            var pedro = new Cliente("Pedro Silveira");
+            var malu = new Cliente("Malu Pereira");
+            var leilao = new Alura.Leilao.Core.Leilao("Obra de Rembrant");
+            leilao.Propoe(new Lance(pedro, 1300));
+            leilao.Propoe(new Lance(malu, 1350));
+            leilao.Propoe(new Lance(joao, 1200));
+
+            var leiloeiro = new Avaliador(leilao);
+            leiloeiro.Avalia();
+
+            Console.WriteLine(leiloeiro.MenorLance);
+            Console.WriteLine(leiloeiro.MaiorLance);
+        }
+    }
+}
