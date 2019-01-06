098053f69fa1423544ba2da5db0edc6d54b3d1d9
dpcosta <dpcosta@gmail.com>
2019-01-06 13:51:54 -0200

    Introduzindo teorias com [Theory]

---
+++ Alura.Leilao.Tests/CalculadoraSomaTestes.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Alura.Leilao.Tests
+{
+    class Calculadora
+    {
+        public int Soma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class CalculadoraTestes
+    {
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        [InlineData(0,1,1)]
+        public void TestaSomaParaDoisNumeros(int num1, int num2, int esperado)
+        {
+            var calc = new Calculadora();
+            var resultado = calc.Soma(num1, num2);
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
--- Alura.Leilao.Tests/LeilaoMelhorLanceTestes.cs
+++ Alura.Leilao.Tests/LeilaoMelhorLanceTestes.cs
@@ -1,77 +1,19 @@
 using Xunit;
 using Alura.Leilao.Core;
-using System.Collections.Generic;
 
 namespace Alura.Leilao.Tests
 {
     public class LeilaoMelhorLanceTestes
     {
-        //primeira tentativa de não repetir código...
-        private Core.Leilao CriaLeilao(string peca, double[] ofertas)
-        {
-            var leilao = new Core.Leilao(peca);
-            var interessado = new Interessado("Fulano", leilao);
-            foreach (var oferta in ofertas)
-            {
-                interessado.Oferece(oferta);
-            }
-            return leilao;
-        }
-
-        [Fact]
-        public void TestaMaiorEMenorLances()
-        {
-            var leilao = CriaLeilao("Obra de Rembrant", new double[] { 1300, 1350, 1200 });
-            var resultado = leilao.Termina();
-            Assert.Equal(1350, resultado.MelhorLance.Valor);
-        }
-
-        [Fact]
-        public void TestaLeilaoComClienteUnico()
-        {
-            var leilao = CriaLeilao("Obra de Rembrant", new double[] { 1200, 1300, 1350, 900 });
-
-            var resultado = leilao.Termina();
-
-            Assert.Equal(1350, resultado.MelhorLance.Valor);
-            Assert.Equal("Fulano", resultado.MelhorLance.Cliente.Nome);
-        }
-
-        [Fact]
-        public void TestaLeilaoComLanceUnico()
-        {
-            var leilao = CriaLeilao("Obra de Rembrant", new double[] { 1200 });
-
-            var resultado = leilao.Termina();
-
-            Assert.Equal(1200, resultado.MelhorLance.Valor);
-        }
-
-        [Fact]
-        public void MaiorLanceRetornaZeroQuandoNaoHaLances()
-        {
-            var leilao = CriaLeilao("Escultura de Aleijadinho", new double[] { });
-
-            var resultado = leilao.Termina();
-
-            Assert.Equal(0, resultado.MelhorLance.Valor);
-        }
-
-        public static IEnumerable<object[]> Ofertas =>
-            new List<object[]>
-            {
-                new object[] { 0 },
-                new object[] { 1200, new double[] { 1200 } },
-                new object[] { 1350, new double[] { 1200, 1350, 1300, 900, 970 } }
-            };
-
         [Theory]
-        [MemberData(nameof(Ofertas))]
-        public void MenorLanceRetornaZeroQuandoNaoHaLances(
+        [InlineData(0, new double[] { })]
+        [InlineData(1200, new double[] { 1200 })]
+        [InlineData(1350, new double[] { 1200, 1300, 1350, 900 })]
+        public void RetornaMaiorOferta(
             double maiorLanceEsperado,
-            params double[] ofertas)
+            double[] ofertas)
         {
-            var leilao = new Core.Leilao("Escultura de Aleijadinho");
+            var leilao = new Core.Leilao("Pintura de Dalí");
             var joao = new Interessado("João de Miranda", leilao);
 
             foreach (var oferta in ofertas)
